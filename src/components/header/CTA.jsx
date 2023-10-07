da96e8440e40587f4dbf221007ef5b9d0e61b713
Mark Louis <marklouisbernardo318@gmail.com>
2023-10-07 08:02:03 +0800

    Small Changes in download CV

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
+// import CV from '../../assets/cv.pdf'
 const CTA = () => {
   return (
     <div className="cta">
-        <a href={CV} download className='btn'>Download CV</a>
+        {/* <a href= download className='btn'>Download CV</a> */}
         <a href="#contact" className='btn btn-primary'>Contact Me</a>
     </div>
   )
